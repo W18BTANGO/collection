ebb83482ba1cec719b686630870d2fe69d4671ed
Matt O'Dea <z5413887@ad.unsw.edu.au>
2025-03-12 13:03:04 +1100

    setting up initial directory structure

---
+++ services/getPriceData.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, Logger } from  //todo
+import axios from 'axios';
+import CONFIGS from ../local.env
+
+const // define Dtos
+
+@Injectable()
+export class getPriceDataService {
+    private readonly logger = new Logger(getPriceDataService.name);
+    private housingDataApiDetails: {
+        dataSourceUrl: string;
+        // other parameters needed to hit the endpoint
+    }
+    // other variables to initialise
+
+
+    constructor(
+        this.housingDataApiDetails = {
+            "dataSourceUrl": 
+        }
+    )
+
+    async doSomething(): Promise<dto> {
+        return "hello world.";
+    }
+}
